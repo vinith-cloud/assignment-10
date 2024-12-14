d9ff92c1f13c592de774a9d7b926f70837cbf89e
vinith-cloud <vinithkumar0710@gmail.com>
2024-12-14 16:47:58 +0530

    js-1

---
+++ index.js
@@ -0,0 +1,101 @@
+ //array
+let array1=["weblanguage","interperated","Dynamic","singlethreadedlanguage"];
+console.log(array1); 
+
+//print
+let a="helloworld";
+ console.log(a);
+
+ let num1=13;
+ let num2=45;
+
+ //function 
+ function add(num1,num2){
+    console.log(`addition is ${num1+num2}`)
+ };
+ add(10,30);
+
+ //airthemetic operators
+
+ let b=47;
+ let c=33;
+//addition 
+ console.log(`${b+c}`);
+ //subtraction
+ console.log(`${b-c}`);
+ //multiplication 
+ console.log(`${b*c}`);
+ //division
+ console.log(`${b/c}`);
+ //modulus
+ console.log(`${b%c}`);
+
+
+// if else
+ 
+let age=18;
+
+if (age >=18){//true
+console.log("eligible for vote");
+}else{//false
+   console.log("not eligible for vote");
+};
+ 
+//for loop
+
+for(let i=0;i<=10;i++){
+   console.log(`condition satisfied..${i}`); 
+}
+
+//array
+
+let array= [10,2,"hello",];
+console.log(array);
+
+array[1]=36;
+console.log(array);
+
+array.push(98,97);
+console.log(array);
+
+array.unshift(24,21);
+console.log(array);
+
+array.pop(97);
+console.log(array);
+
+array.shift(24);
+console.log(array);
+
+//object
+ 
+let object={
+   name:"vinith kumar",
+   age:24,
+   designation:"B.sc",
+   mobile :9751905552,
+}
+console.log(object.name);
+
+object.name="vinith kumar nallasamy"
+console.log(object);
+
+object.profession="full-stack developer";
+console.log(object);
+
+delete object.mobile;
+console.log(object);
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
